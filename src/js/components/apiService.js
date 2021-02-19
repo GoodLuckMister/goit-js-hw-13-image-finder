e0ea7cf5f56b773a9f08c97bb9231f597fa90b62
GodLuckMister <paet14@gmail.com>
2021-02-19 19:36:49 +0200

    new update main js and api service

--- src/js/components/apiService.js
+++ src/js/components/apiService.js
@@ -1,9 +1,31 @@
-const apiKey = '20328983-97c171d0912a39316d7306b5c';
+export default class apiService {
+  constructor() {
+    this.searchQuery = '';
+    this.key = '20328983-97c171d0912a39316d7306b5c';
+    this.page = 1;
+  }
 
-export default fetch(
-  `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=black&page=1&per_page=12&key=${apiKey}`,
-)
-  .then(response => {
-    return response.json();
-  })
-  .catch(console.log);
+  fetchApiService() {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.key}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        return data.hits;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    return (this.searchQuery = newQuery);
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,31 @@
 import apiImages from './components/apiService';
 import templateGallery from '../templates/gallery.hbs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const newApiImages = new apiImages();
 const refs = {
   galleryList: document.querySelector('.gallery'),
   input: document.querySelector('.search-form'),
+  buttonSubmit: document.querySelector('.search-form__submit'),
+  buttonLoad: document.querySelector('.load-more'),
 };
 
-// function renderImageList(img) {
-//   const markup = templateGallery(img);
-//   refs.galleryList.innerHTML = markup;
-// }
+refs.input.addEventListener('submit', onSearch);
+refs.buttonLoad.addEventListener('click', onLoadMore);
 
-apiImages.then(r => {
-  return (refs.galleryList.innerHTML = templateGallery(r.hits));
-});
+function onSearch(e) {
+  e.preventDefault();
+
+  newApiImages.query = e.currentTarget.elements.query.value;
+
+  newApiImages.resetPage();
+  newApiImages.fetchApiService().then(appendImagesMarkup);
+  refs.galleryList.innerHTML = '';
+}
+function onLoadMore() {
+  newApiImages.fetchApiService().then(appendImagesMarkup);
+}
+
+function appendImagesMarkup(images) {
+  refs.galleryList.insertAdjacentHTML('beforeend', templateGallery(images));
+}
