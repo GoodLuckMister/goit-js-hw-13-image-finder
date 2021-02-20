8643e632459109c72192165df72fd0f0cc7b51a1
GodLuckMister <paet14@gmail.com>
2021-02-20 15:24:58 +0200

    update new button load-more

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,52 @@
 import apiImages from './components/apiService';
 import templateGallery from '../templates/gallery.hbs';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoadMore from './components/buttonLoad';
 
 const newApiImages = new apiImages();
 const refs = {
   galleryList: document.querySelector('.gallery'),
   input: document.querySelector('.search-form'),
   buttonSubmit: document.querySelector('.search-form__submit'),
-  buttonLoad: document.querySelector('.load-more'),
+  // buttonLoad: document.querySelector('.load-more'),
 };
 
+const loadMoreButton = new LoadMore({
+  selector: '.load-more',
+  hidden: true,
+});
+
+console.log(loadMoreButton);
+
 refs.input.addEventListener('submit', onSearch);
-refs.buttonLoad.addEventListener('click', onLoadMore);
+loadMoreButton.refs.button.addEventListener('click', fetchImages);
 
 function onSearch(e) {
   e.preventDefault();
 
   newApiImages.query = e.currentTarget.elements.query.value;
+  loadMoreButton.show();
 
   newApiImages.resetPage();
-  newApiImages.fetchApiService().then(appendImagesMarkup);
-  refs.galleryList.innerHTML = '';
+  clearGallery();
+  fetchImages();
 }
-function onLoadMore() {
-  newApiImages.fetchApiService().then(appendImagesMarkup);
+
+function fetchImages() {
+  loadMoreButton.disable();
+  setTimeout(() => {
+    newApiImages.fetchApiService().then(img => {
+      appendImagesMarkup(img);
+
+      loadMoreButton.enable();
+    });
+  }, 1000);
 }
 
 function appendImagesMarkup(images) {
   refs.galleryList.insertAdjacentHTML('beforeend', templateGallery(images));
+}
+
+function clearGallery() {
+  refs.galleryList.innerHTML = '';
 }
